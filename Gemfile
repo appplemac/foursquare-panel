d4447698f45e7a5e98bc53692f3c7579bfe2c116
Alexey <appplemac@gmail.com>
2013-09-08 16:53:08 +0200

    Auth

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 gem "sinatra"
 gem "foursquare2"
+gem "rack-flash"
--- app.rb
+++ app.rb
@@ -3,41 +3,48 @@ require 'foursquare2'
 
 enable :sessions
 
+get '/' do
+  redirect('/edit')
+end
+
 get '/edit' do
+  unless session[:token]
+    redirect('/redirect')
+  end
+
   erb :edit
 end
 
 post '/edit' do
-  unless session[:token]
-    redirect("/redirect")
+  if session[:token].empty?
+    redirect('/edit')
   end
-
   @data = params[:data]
 
-  options = {}
-  options[:name] = @data["name"]
-  options[:address] = @data["address"]
-  options[:crossStreet] = @data["crossStreet"]
-  options[:city] = @data["city"]
-  options[:state] = @data["state"]
-  options[:zip] = @data["zip"]
-  options[:phone] = @data["phone"]
-  options[:ll] = @data["ll"]
-  options[:primaryCategoryId] = @data["primaryCategoryId"]
-
-  options.reject! {|k,_| options[k].empty?}
-
   @venues = @data["venue_id"].detete(" ").split(",")
-
-  raise(ArgumentError, "No venues provided") if @venues.empty?
-  raise(ArgumentError, "Invalid token") if session[:token].empty?
-
-  client = Foursquare2::Client.new(:oauth_token => session[:token])
-
-  @venues.each do |venue|
-    client.propose_venue_edit(venue, options)
+  if @venues.empty?
+    flash[:notice] = "You have provided no venues for edition"
+    redirect('/edit')
   end
 
+  @options = {}
+  @options[:name] = @data["name"]
+  @options[:city] = @data["city"]
+  @options[:state] = @data["state"]
+  @options[:phone] = @data["phone"]
+  @options[:primaryCategoryId] = @data["cat"]
+  @options.reject! {|_,value| value.empty?}
+
+  begin
+    client = Foursquare2::Client.new(:oauth_token => session[:token])
+
+    @venues.each do |venue|
+      client.propose_venue_edit(venue, @options)
+    end
+  rescue APIError => e
+    flash[:notice] = e.message
+    redirect('/edit')
+  end
 end
 
 get '/redirect' do
