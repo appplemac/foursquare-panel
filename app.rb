7a5f65ed5dba0b78763cf3da2114afe7bf5f0e5e
Alexey <appplemac@gmail.com>
2013-09-23 20:00:52 +0200

    Created the Venue class

--- app.rb
+++ app.rb
@@ -20,9 +20,25 @@ class Venue
     end
   end
 
-  def edit(client)
-    # TODO: THIS IS WRONG
-    client.propose_venue_edit(@venue_id)
+  def to_h
+    props = {}
+    [:name, :city, :state, :phone].each do |attr|
+      unless self.send(attr).nil?
+        props[attr] = self.send(attr)
+      end
+    end
+    # special cases
+    unless self.cat_id.nil?
+      props[:primaryCategoryId] = self.cat_id
+    end
+    props
+  end
+
+  def edit!(client)
+    unless client.is_a?(Foursquare2::Client)
+      raise ArgumentError, "Client looks invalid"
+    end
+    client.propose_venue_edit(@venue_id, self.to_h)
   end
 end
 
